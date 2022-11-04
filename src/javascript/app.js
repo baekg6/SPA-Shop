b3fa18f754c947700613a96af660a34e35ff4927
Gyeonghyeon Baek <baekg.dev@gmail.com>
2022-11-05 01:35:00 +0900

    Feat : 상품 목록 페이지-상품 상세 페이지 연결(단일)

---
+++ src/javascript/app.js
@@ -0,0 +1,17 @@
+import { ProductPage, ProductDetail } from "./pages/index.js";
+import { Router } from "./utils/index.js";
+
+export default class App {
+    constructor(props) {
+        this.props = props;
+    }
+    async setup() {
+        const { el } = this.props; //구조 분해 할당(#root)
+
+        const router = new Router({
+            "/": ProductPage,
+            "/detail": ProductDetail,
+        });
+        router.init(el);
+    }
+}
---
+++ src/javascript/pages/index.js
@@ -0,0 +1,3 @@
+// 페이지를 연결(구현X)
+export { default as ProductPage } from "./productPage.js";
+export { default as ProductDetail } from "./productDetail.js";
